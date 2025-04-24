f5f0dc07c6dc7040c8aa5e64ad2e46203405b853
AmarBahadurThapa619 <thapaamar9867@gmail.com>
2025-04-24 13:41:34 +0545

    Deleted the script.js
    Deleted the scrpit.js due to problem in console.log

--- script.js
+++
@@ -1,6 +0,0 @@
-let myVariableName = 'Laptop';
-conssole.log(myVariableName);
-let result = 10 + 20;
-console.log(result);
-let userInputNumber = prompt('Enter your Number');
-console.log(userInputNumber);
